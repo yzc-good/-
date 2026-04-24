83a334bcdaf97163e645dd874b53cf1aa65c59aa
yzc <13061208783@163.com>
2026-04-24 09:53:19 +0800

    Add direction array for grid navigation
    Implement a direction array for movement in a grid.

---
+++ 方向数组使用.cpp
@@ -0,0 +1,44 @@
+利用方向数组
+int dx[N] = { 0,1,0,-1 };
+int dy[N] = { 1,0,-1,0 };
+和pos的结合就可以实现上、下、左、右的移位
+//-----------------------------------------------------------------------------
+#include<iostream>
+using namespace std;
+const int N = 30;
+int arr[N][N];
+int dx[N] = { 0,1,0,-1 };
+int dy[N] = { 1,0,-1,0 };
+
+int main()
+{
+    int n;
+    cin >> n;
+    int pos = 0;
+    int x = 1, y = 1;
+    for (int i = 1; i <= n * n; i++)
+    {
+        arr[x][y] = i;
+        //计算接下来的位置
+        int a = x + dx[pos];
+        int b = y + dy[pos];
+        if (a<1 || a>n || b<1 || b>n || arr[a][b])
+        {
+            pos = (pos + 1) % 4;//这句代码实现了方向的变换。
+            a = x + dx[pos];
+            b = y + dy[pos];
+
+        }
+        x = a, y = b;//更新x,y的值
+
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            printf("%3d", arr[i][j]);
+        }
+        cout << endl;
+    }
+    return 0;
+}
